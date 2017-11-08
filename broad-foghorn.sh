5ab63baf2662c636974b56510e4977c6dc67d54e
clint <cgilbert@broadinstitute.org>
2017-11-08 18:01:02 -0500

    Prevent output from reuse from ending up in the resulting cvcf by passing the -q flag

--- broad-foghorn.sh
+++ broad-foghorn.sh
@@ -2,9 +2,9 @@
 
 source /broad/software/scripts/useuse
 
-reuse Tabix
-reuse .boost-1.55.0
-reuse .fftw-3.3.4
-reuse GCC-4.9
+reuse -q Tabix 
+reuse -q .boost-1.55.0
+reuse -q .fftw-3.3.4
+reuse -q GCC-4.9
 
 ./foghorn "$@"
